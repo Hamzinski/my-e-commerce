c2c48a0c5a02a24fb8b2e5339c762fba00585949
Muhammed Hamza Toptancı <toptanci.hamza@gmail.com>
2024-02-26 18:50:41 +0300

    Form-Update

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,72 +1,195 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 
-const api = axios.create({
+const instance = axios.create({
   baseURL: "https://workintech-fe-ecommerce.onrender.com",
 });
 
+const initialData = {
+  name: "",
+  password: "",
+  confirmPassword: "",
+  email: "",
+  role_id: "3",
+  store: {
+    phone: "",
+    name: "",
+    tax_no: "",
+    bank_account: "",
+  },
+};
+
 function SignUpPage() {
-  const { handleSubmit, register, setError, setValue, watch } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm({ defaultValues: initialData });
 
-  const watchPassword = watch("password", "");
+  const [roles, setRoles] = useState([]);
 
-  const onSubmit = async (data) => {
-    try {
-      const response = await api.post("/signup", data);
-      console.log(response.data);
-    } catch (error) {
-      console.error("An error occurred while submitting the form", error);
-    }
+  useEffect(() => {
+    instance.get("/roles").then((res) => {
+      setRoles(res.data);
+      console.log("resp:", res.data);
+    });
+  }, []);
+
+  const selectedRole = watch("role_id");
+  const password = watch("password");
+
+  const onSubmit = (data) => {
+    const { confirmPassword, ...postData } = data;
+    instance.post("/signup", postData).then((res) => {
+      console.log(res);
+    });
   };
 
   return (
     <div className="flex justify-center">
-      <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          name="username"
-          id="username"
-          {...register("username", { required: true, minLength: 3 })}
-        />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            {...register("name", {
+              required: "Name is required",
+              minLength: {
+                value: 3,
+                message: "Name must be at least 3 characters long",
+              },
+            })}
+          />
+          <span>{errors.name && errors.name.message}</span>
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /.*@.*\..*/,
+                message: "Enter a valid email address",
+              },
+            })}
+          />
+          <span>{errors.email && errors.email.message}</span>
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 8,
+                message: "Password must be at least 8 characters long",
+              },
+              pattern: {
+                value: /^(?=.*[a-z])[A-Za-z\d@$!%*?&]+$/,
+                message:
+                  "Password should include at least one letter, one number, and one special character",
+              },
+            })}
+          />
+          <span>{errors.password && errors.password.message}</span>
+        </div>
+        <div>
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            {...register("confirmPassword", {
+              validate: (value) =>
+                value === password || "Passwords do not match",
+            })}
+          />
+          <span>
+            {errors.confirmPassword && errors.confirmPassword.message}
+          </span>
+        </div>
+        <div>
+          <label>Role</label>
+          <select {...register("role_id")}>
+            {roles.map((role_id) => (
+              <option key={role_id.id} value={role_id.id}>
+                {role_id.name}
+              </option>
+            ))}
+          </select>
+        </div>
 
-        <label htmlFor="email">E-mail</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          {...register("email", {
-            required: true,
-            pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-          })}
-        />
+        {selectedRole === "2" && (
+          <>
+            <div>
+              <label>Store Name</label>
+              <input
+                type="text"
+                {...register("store.name", {
+                  required: "Store Name is required",
+                  minLength: {
+                    value: 3,
+                    message: "Store Name must be at least 3 characters long",
+                  },
+                })}
+              />
+              <span>{errors.store?.name && errors.store.name.message}</span>
+            </div>
+            <div>
+              <label>Store Phone</label>
+              <input
+                type="text"
+                {...register("store.phone", {
+                  required: "Store Phone is required",
+                  pattern: {
+                    value: /^\+?\d{10,14}$/,
+                    message: "Enter a valid Türkiye phone number",
+                  },
+                })}
+              />
+              <span>{errors.store?.phone && errors.store.phone.message}</span>
+            </div>
+            <div>
+              <label>Store Tax ID</label>
+              <input
+                type="text"
+                {...register("store.tax_no", {
+                  required: "Store Tax ID is required",
+                  pattern: {
+                    value: /^T\d{4}V\d{6}$/,
+                    message: "Enter a valid Tax ID (e.g., T1234V567890)",
+                  },
+                })}
+              />
+              <span>{errors.store?.tax_no && errors.store.tax_no.message}</span>
+            </div>
+            <div>
+              <label>Store Bank Account</label>
+              <input
+                type="text"
+                {...register("store.bank_account", {
+                  required: "Store Bank Account is required",
+                  pattern: {
+                    // Add your IBAN validation pattern here
+                    value: /^[A-Z]{2}\d{2}[A-Za-z0-9]{1,30}$/,
+                    message: "Enter a valid IBAN address",
+                  },
+                })}
+              />
 
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          {...register("password", {
-            required: true,
-            minLength: 8,
-          })}
-        />
+              <span>
+                {errors.store?.bank_account &&
+                  errors.store.bank_account.message}
+              </span>
+            </div>
+          </>
+        )}
 
-        <label htmlFor="passwordAgain">Password again</label>
-        <input
-          type="password"
-          name="passwordAgain"
-          id="passwordAgain"
-          {...register("passwordAgain", {
-            required: true,
-            validate: (value) =>
-              value === watchPassword || "Passwords do not match",
-          })}
-        />
-        <button className="bg-primary-bg" type="submit">
-          Submit
-        </button>
+        <div>
+          <button type="submit">Sign Up</button>
+        </div>
       </form>
     </div>
   );
